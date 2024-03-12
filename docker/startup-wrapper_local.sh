f02b80a62f4fd19329a24836f999ced10e7e8548
Biblink <alby.bot.24@gmail.com>
2024-03-12 18:45:31 +0100

    chore: added docker

---
+++ docker/startup-wrapper_local.sh
@@ -0,0 +1,11 @@
+# Required by maatwebsite/excel
+sudo apk add php82-xmlreader
+
+composer install
+php artisan migrate
+
+chmod +x /scripts/scheduler.sh
+/scripts/scheduler.sh &
+
+sudo chmod +x /usr/local/bin/startup.sh
+/usr/local/bin/startup.sh
