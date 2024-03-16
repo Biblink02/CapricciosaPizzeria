0433cbae83a80bb5e9c06696d149cbd7e55d997b
Biblink02 <alby.bot.24@gmail.com>
2024-03-16 11:28:44 +0100

    fix: now the app is configured and it's using typescript

---
+++ src/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/ts/**/*.vue',
+    ],
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+} satisfies Config;
