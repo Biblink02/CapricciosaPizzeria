ab43d65febb556dcc380b50213a12b352922e426
farah <rouanfarah@gmail.com>
2024-03-20 00:18:10 +0100

    feat: footer

--- src/resources/ts/Components/FooterComponent.vue
+++ src/resources/ts/Components/FooterComponent.vue
@@ -1,25 +1,45 @@
 <script setup lang="ts">
 
+import {DisclosureButton} from "@headlessui/vue";
+
 const elements = [
     {
-        name: "aaa",
+        name: "Enoteca Centrale",
         lines: [
-            "aaa",
-            "aaa"
+            "Via IV Novembre, 59",
+            "35035 Mestrino Padova",
+            "Tel. +39 049 9004947",
+            "Mobile +39 335 5717527 (WhatsApp)",
+            "info@enotecacentrale.it"
         ]
     },
     {
-        name: "bbb",
+        name: "News/Eventi",
         lines: [
-            "bbb",
-            "bbb"
+            "Banqueting",
+            "Eventi",
+            "Events",
+            "Foto Gallery Eventi",
+            "News"
         ]
     },
     {
-        name: "ccc",
+        name: "Contatti",
         lines: [
-            "ccc",
-            "ccc"
+            "Dove siamo"
+        ]
+    },
+    {
+        name: "Orari apertura",
+        lines: [
+            "Lunedì - Sabato",
+            "Aperitivo",
+            "dalle 11:00 alle 15:00 e",
+            "dalle 17:30 alle 23.30",
+            "Servizio pranzo",
+            "dalle 12:00 alle 14.30",
+            "Servizio cena",
+            "dalle 19:00 alle 23:30"
         ]
     }
 ]
@@ -34,9 +54,15 @@ const elements = [
             <hr class="w-full">
             <div>
                 <p v-for="line in element.lines">
-                    {{line}}
+                    {{ line}}
                 </p>
             </div>
         </div>
     </footer>
+    <hr class="w-full">
+    <div class="footer flex flex-row space-x-10 justify-center my-4" >
+        ©Enoteca Centrale S.r.l. | Dati Societari | Privacy GDPR | Credits
+    </div>
 </template>
+
+
