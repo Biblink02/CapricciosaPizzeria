4e25a1d4b457a2b2244d1993324b7f2fb29a4a5c
Biblink02 <alby.bot.24@gmail.com>
2024-04-06 11:42:54 +0200

    chore: fix for graphic

--- src/resources/ts/Pages/Menus/Dish.vue
+++ src/resources/ts/Pages/Menus/Dish.vue
@@ -4,24 +4,33 @@
 const props = defineProps<{
     dish: Dish
 }>()
+
 </script>
 
 <template>
-    <div class="flex justify-center text-lg">
-        <strong>{{ $t(dish.name) }}&nbsp</strong>
-        <div v-for="(allergen, index) in dish.allergens" class="inline-flex text-xs text-gray-400">
-            {{ allergen.number }}
-            <span v-if="index < dish.allergens.length - 1">,&nbsp;</span>
+    <div class="w-full">
+        <!-- NOME + ALLERGENI -->
+        <div class="flex justify-center text-lg w-full">
+            <!-- (NOME) -->
+            <strong class="lg:max-w-[80%] md:max-w-[85%] sm:max-w-[90%] text-center w-min-[80%]">{{ $t(dish.name) }}&nbsp</strong>
+            <!-- (ALLERGENI) -->
+            <div v-for="(allergen, index) in dish.allergens" class="inline-flex text-xs text-gray-400">
+                {{ allergen.number }}
+                <span class="lg:max-w-[80%] md:max-w-[85%] sm:max-w-[90%] text-center" v-if="index < dish.allergens.length - 1">,&nbsp;</span>
+            </div>
         </div>
-    </div>
-    <div class="flex justify-center">
-        <div v-for="(ingredient, index) in dish.ingredients" class="inline-flex">
-            {{ $t(ingredient.name) }}
-            <span v-if="index < dish.ingredients.length - 1">,&nbsp;</span>
+        <!-- INGREDIENTI -->
+        <div class="flex justify-center">
+            <div class="lg:max-w-[80%] md:max-w-[85%] sm:max-w-[90%] text-center">
+                <div v-for="(ingredient, index) in dish.ingredients" class="inline-flex">
+                    {{ $t(ingredient.name) }}
+                    <span v-if="index < dish.ingredients.length - 1">,&nbsp;</span>
+                </div>
+            </div>
         </div>
+        <!-- PREZZO -->
+        <div class="flex justify-center pt-3 w-full"> €&nbsp; {{ $t(dish.pivot.price) }}</div>
     </div>
-    <div class="flex justify-center pt-3"> €&nbsp; {{ $t(dish.pivot.price) }}</div>
-
 </template>
 
 <style scoped>
