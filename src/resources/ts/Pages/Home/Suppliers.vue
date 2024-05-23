7a1e3c40ee678b3f52572b3b8e3df43c61667235
Fireentity <croceclaudio57@gmail.com>
2024-05-23 16:06:02 +0200

    feat: homepage

--- src/resources/ts/Pages/Home/Suppliers.vue
+++ src/resources/ts/Pages/Home/Suppliers.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 
+import route from "ziggy-js";
+
+
 const props = defineProps<{
     suppliers: Supplier[]
 }>()
@@ -8,26 +11,25 @@ const props = defineProps<{
 
 <template>
 
-    <div class="bg-white">
-        <div class="mx-auto max-w-7xl px-6">
-            <div class="lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
-                <div>
-                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{$t('Le nostre eccellenze')}}</h2>
-                    <p class="mt-3 max-w-3xl text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.</p>
-                    <div class="mt-8 sm:flex">
-                    </div>
+    <div>
+        <div class="grid place-content-center">
+
+            <div>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{$t('Le nostre eccellenze')}}</h2>
+                <p class="mt-3 max-w-3xl text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.</p>
+                <div class="mt-8 sm:flex">
                 </div>
-                <div class=" mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-                    <template v-for="supplier in suppliers">
-                        <div class="col-span-1 flex justify-center py-8 px-8">
-                            <img
-                                class="max-h-12"
-                                :src="'images/'+ supplier?.img_url"
-                                :alt="supplier.name"
-                            >
-                        </div>
-                    </template>
-                </div>
+            </div>
+
+
+            <div class="mx-auto space-x-8 space-y-10 max-w-5xl flex flex-row flex-wrap justify-around place-content-centeram" >
+                <template v-for="supplier in suppliers">
+                    <img
+                        class="w-1/6 h-auto object-scale-down"
+                        :src="'images/'+ supplier?.img_url"
+                        :alt="supplier.name"
+                    >
+                </template>
             </div>
         </div>
     </div>
