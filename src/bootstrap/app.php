c535f0e7dc5dfa1b70f967a5fe73f50ff27976e7
Biblink02 <alby.bot.24@gmail.com>
2025-02-16 14:42:02 +0100

    fix: trust proxy for production

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -18,10 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        $middleware->trustProxies(at: [
-            '192.168.220.4'
-        ]);
+        if(env('APP_ENV') === 'production') {
+            env('APP_TRUST_PROXY') ?? $middleware->trustProxies(at: [
+                env('APP_TRUST_PROXY')
+            ]);
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
